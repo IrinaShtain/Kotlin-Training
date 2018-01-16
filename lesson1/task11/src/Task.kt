51fd34903095d43e35ff7d0c581eea4a2ec05404
IrinaShtain <shtainirina@gmail.com>
2018-01-16 10:13:04 +0200

    introduction is done

--- lesson1/task11/src/Task.kt
+++ lesson1/task11/src/Task.kt
@@ -1,7 +1,10 @@
 import java.util.*
 
+
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList, { p0, p1 ->
+        p1 - p0
+    })
     return arrayList
 }
